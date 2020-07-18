81ec07d8b2b9612f4204e8961eaad56e9bd69387
Sakshi1520 <58680810+Sakshi1520@users.noreply.github.com>
2020-07-18 22:04:15 +0530

    connected staff details form to  db

--- staff_register.php
+++ staff_register.php
@@ -2,11 +2,14 @@
 
 include('connect.php');
 extract($_POST);
-
+// echo("hi");
 if(isset($_POST['submit'])){
-    echo("hehe");
-    $query = "insert into `staff_details` (`first_name`, `last_name`, `password`) values ('$firstname', '$lastname', '$pwd')";
+    // echo("hehe");
+    // $query = "insert into `staff_details` (`first_name`, `last_name`, `password`) values ('$firstname', '$lastname', '$pwd')";
+
+    $query = "INSERT INTO `staff_details`(`regis_no`, `name`, `age`, `sex`, `cheque_no`, `mobile_no`, `designation`, `department`, `depot`, `addictions`, `address`, `comorbidity`, `last_working`, `travel_history`, `exposure`, `symptoms`, `testing_date`, `hospital`, `admission_date`, `oxygen`, `discharge`, `dis_date`, `dis_no`, `family`, `followup`) VALUES ('$regis_no', '$name', '$age', '$sex', '$cheque_no', '$mobile_no', '$designation', '$department', '$depot', '$addictions', '$address', '$comorbidity', '$last_working', '$travel_history', '$exposure', '$symptoms', '$testing_date', '$hospital', '$admission_date', '$oxygen', '$discharge', '$dis_date', '$dis_no', '$family', '$followup')";
     $result = mysqli_query($conn,$query);
-    header('location:staff_details.php');
+    // echo $query;
+    header('location:staff_details.html');
 }
 ?>
